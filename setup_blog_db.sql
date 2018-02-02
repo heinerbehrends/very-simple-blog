e62cfb163b5cb514b8ad64eaa17da62b076a541e
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-02 13:38:19 +0100

    Added references. Added table that references the article and the category ids.

--- setup_blog_db.sql
+++ setup_blog_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE posts (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   title VARCHAR(256) NOT NULL,
   post TEXT NOT NULL,
-  category TEXT NOT NULL,
+  category_id INT UNSIGNED NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -14,4 +14,15 @@ CREATE TABLE categories (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   category TEXT NOT NULL
   PRIMARY KEY (id)
+);
+
+ALTER TABLE posts ADD FOREIGN KEY (category_id) REFERENCES categories(id);
+
+CREATE TABLE articles_categories (
+id INT NOT NULL AUTO_INCREMENT,
+article_id INT NOT NULL,
+category_id INT NOT NULL,
+PRIMARY KEY (ID),
+FOREIGN KEY (article_id) REFERENCES articles(id),
+FOREIGN KEY (category_id) REFERENCES categories(id)
 );
