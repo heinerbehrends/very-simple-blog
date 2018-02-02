5249067358226b87a6fb9a144e69334592fef19b
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-02 13:34:55 +0100

    implemented getCategoryPosts() function that shows only the posts belonging to a particular category.

--- getBlogPosts.js
+++ getBlogPosts.js
@@ -1,11 +1,31 @@
 $(function() {
+
+  function makeCategoryListItem(category_id, category) {
+    return '<li id="' + category_id + '" class="list-group-item border-0 category-menu">' + category + '</li>';
+  }
+  var categoriesListElement = $("#selectCategory");
+
   function makePost(title, post) {
     return "<h1>" + title + "</h1> <p>" + post + "</p>";
   }
   var postsElement = $("#posts");
+
   $.ajax({
     method: "GET",
-    url: "api.php",
+    url: "categories_api.php",
+  }).done(function(data) {
+    $.each(data, function(key, value) {
+      var category = value["category"];
+      var category_id = value["id"];
+      var categoryListItem = makeCategoryListItem(category_id, category);
+      categoriesListElement.append(categoryListItem);
+    })
+    getCategoryPosts();
+  })
+
+  $.ajax({
+    method: "GET",
+    url: "api.php?category=ALL",
   }).done(function(data) {
     $.each(data, function(key, value) {
       var title = value["title"];
@@ -14,29 +34,30 @@ $(function() {
       postsElement.append(post);
     })
   })
-  $("li.list-group-item").click(function() {
-    var category = $(this).html().toLowerCase();
-    postsElement.html("");
-    console.log("api.php?category=" + category);
-    $.ajax({
-      method: "GET",
-      url: "api.php?category=" + category,
-      success: function(data){
+
+  function getCategoryPosts() {
+    $(".category-menu").click(function() {
+      console.log($(this));
+      var category = $(this)[0].getAttribute("id");
+      postsElement.html("");
+      console.log("api.php?category=" + category);
+      $.ajax({
+        method: "GET",
+        url: "api.php?category=" + category,
+        success: function(data){
+          console.log("Hello!");
+        }
+      }).done(function(data) {
         console.log("Hello!");
-      }
-    }).done(function(data) {
-      console.log("Hello!");
-      console.log(data);
+        console.log(data);
 
-      $.each(data, function(key, value) {
-
-        var postCategory = value["category"];
-        if (postCategory === category.toLowerCase()) {
+        $.each(data, function(key, value) {
+          var postCategory = value["category"];
           var post = makePost(value["title"], value["post"]);
           postsElement.append(post);
-        }
+        })
       })
     })
-  })
+  }
 
 })
