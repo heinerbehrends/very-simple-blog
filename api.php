cafac514090feffff9b90eda098346dcaab007b8
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-02 12:03:08 +0100

    added break statement to switch. changed real_escape_string to procedural notation. Changed sql statement logic to include ALL.

--- api.php
+++ api.php
@@ -12,18 +12,20 @@ $request_method = $_SERVER["REQUEST_METHOD"];
 
 switch ($request_method) {
   case "POST":
-  handlePostRequest($conn);
+    handlePostRequest($conn);
+    break;
   case "GET":
-  handleGetRequest($conn);
+    handleGetRequest($conn);
+    break;
 }
 $conn->close();
 
 
 function handlePostRequest($conn) {
   // print_r($_POST);
-  $title = mysqli_real_escape_string($_POST['title']);
-  $post = mysqli_real_escape_string($_POST['post']);
-  $category = mysqli_real_escape_string($_POST['category']);
+  $title = $conn->real_escape_string($_POST['title']);
+  $post = $conn->real_escape_string($_POST['post']);
+  $category = $conn->real_escape_string($_POST['category']);
   $sql = "INSERT INTO posts (title, post, category) VALUES (?, ?, ?)";
   $statement = $conn->prepare($sql);
   $statement->bind_param("sss", $title, $post, $category);
@@ -33,8 +35,12 @@ function handleGetRequest($conn) {
   // debug_to_console(array_key_exists("category", $_GET));
   $category = $_GET["category"];
   // echo $category;
-
-  $sql = "SELECT * FROM posts WHERE category = '$category' ORDER BY id DESC";
+  if ($category === "ALL") {
+    $sql = "SELECT * FROM posts";
+  }
+  else {
+    $sql = "SELECT * FROM posts WHERE category = '$category' ORDER BY id DESC";
+  }
   $result = $conn->query($sql);
   $result_array = array();
   if ($result->num_rows > 0) {
@@ -45,11 +51,11 @@ function handleGetRequest($conn) {
   // echo a json encoded array to the client
   echo json_encode($result_array);
 }
-function debug_to_console($data) {
-  $output = $data;
-  if (is_array($output))
-  $output = implode(',', $output);
-
-  echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
-}
+// function debug_to_console($data) {
+//   $output = $data;
+//   if (is_array($output))
+//   $output = implode(',', $output);
+//
+//   echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+// }
 ?>
