4b5266be4ca27b2a93dc261b7303862874b6e568
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-05 21:11:30 +0100

    Refactored code. Created function replaceText($domInput, $rulesObject).

--- textExpander.js
+++ textExpander.js
@@ -1,17 +1,20 @@
 $(function() {
+  function replaceText($domInput, $rulesObject) {
+    $domInput.keyup(function() {
+      var userInput = $domInput.val();
+      for (var rule in $rulesObject) {
+        if(userInput.indexOf(rule) !== -1) {
+          var replaceString = userInput.replace(rule, $rulesObject[rule]);
+          $domInput.val(replaceString);
+        }
+      }
+    })
+  }
   var textExpansionRules = {
     'cg': 'Code Gorilla',
     'gn': 'Groningen'
   }
   var textArea = $("#text-area");
-  textArea.keyup(function() {
-    var userInput = textArea.val();
-    for (var rule in textExpansionRules) {
-      if(userInput.indexOf(rule) !== -1) {
-        var replaceString = userInput.replace(rule, textExpansionRules[rule]);
-        textArea.val(replaceString);
-      }
 
-    }
-  })
+  textArea.keyup(replaceText(textArea, textExpansionRules));
 })
