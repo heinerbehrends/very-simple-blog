1429a81268c48bee8437c9fdc9b746afa9005b31
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-05 15:46:56 +0100

    [W3-006] Change api.php so that posts are filtered by category using the N:M relation.

--- api.php
+++ api.php
@@ -12,11 +12,11 @@ $request_method = $_SERVER["REQUEST_METHOD"];
 
 switch ($request_method) {
   case "POST":
-    handlePostRequest($conn);
-    break;
+  handlePostRequest($conn);
+  break;
   case "GET":
-    handleGetRequest($conn);
-    break;
+  handleGetRequest($conn);
+  break;
 }
 $conn->close();
 
@@ -30,9 +30,9 @@ function handlePostRequest($conn) {
   // print_r($categoriesArray);
   $title = $conn->real_escape_string($_POST['title']);
   $post = $conn->real_escape_string($_POST['post']);
-  $sql = "INSERT INTO posts (title, post, category_id) VALUES (?, ?, ?)";
+  $sql = "INSERT INTO posts (title, post) VALUES (?, ?)";
   $statement = $conn->prepare($sql);
-  $statement->bind_param("sss", $title, $post, $category);
+  $statement->bind_param("ss", $title, $post);
   $statement->execute();
 
   $sqlGetMaxID = "SELECT * FROM posts ORDER BY id DESC LIMIT 1";
@@ -50,26 +50,46 @@ function handleGetRequest($conn) {
   $category = $_GET["category"];
   // echo $category;
   if ($category === "ALL") {
-    $sql = "SELECT * FROM posts";
+    $sql_get_posts_by_category = "SELECT * FROM posts ORDER BY id DESC";
   }
   else {
-    $sql = "SELECT * FROM posts WHERE category_id = '$category' ORDER BY id DESC";
+    $sql_get_post_ids = "SELECT * FROM articles_categories WHERE category_id = '$category' ORDER BY id DESC";
+    $result = $conn->query($sql_get_post_ids);
+    $result_array = array();
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        array_push($result_array, $row);
+      }
+    }
+    // get post id by category
+    $posts_by_category_array = array();
+    foreach ($result_array as $result) {
+      array_push($posts_by_category_array, $result["article_id"]);
+    }
+    // print_r($posts_by_category_array);
+    $array_keys = array_keys($posts_by_category_array);
+    $last_key = array_pop($array_keys);
+    $posts_string = "(";
+    foreach($posts_by_category_array as $index => $post_id) {
+      if ($index != $last_key) {
+        $posts_string = $posts_string . "{$post_id}, ";
+      }
+      else {
+        $posts_string = $posts_string . "{$post_id}";
+      }
+    }
+    $posts_string = $posts_string . ")";
+    $sql_get_posts_by_category = "SELECT * from posts WHERE id in $posts_string";
   }
-  $result = $conn->query($sql);
-  $result_array = array();
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      array_push($result_array, $row);
+  $sql_result_posts_by_category = $conn->query($sql_get_posts_by_category);
+  $result_array_posts_by_category = array();
+  if ($sql_result_posts_by_category->num_rows > 0) {
+    while($row = $sql_result_posts_by_category->fetch_assoc()) {
+      array_push($result_array_posts_by_category, $row);
     }
   }
+
   // echo a json encoded array to the client
-  echo json_encode($result_array);
+  echo json_encode($result_array_posts_by_category);
 }
-// function debug_to_console($data) {
-//   $output = $data;
-//   if (is_array($output))
-//   $output = implode(',', $output);
-//
-//   echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
-// }
 ?>
