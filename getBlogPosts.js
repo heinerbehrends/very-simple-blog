569c0ed6cc2fb1911fcefb376e9faa147e242431
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-06 17:32:50 +0100

    Added comment form to makePost() Added submit handler that sends comment and article id to comments_api.php

--- getBlogPosts.js
+++ getBlogPosts.js
@@ -4,9 +4,16 @@ $(function() {
     return '<li id="' + category_id + '" class="list-group-item border-0 category-menu">' + category + '</li>';
   }
   var categoriesListElement = $("#selectCategory");
-  
-  function makePost(title, post) {
-      return '<h2 class="mb-0">' + title + "</h2> <p style='font-size: 1.2rem' class='mb-5'>" + post + '</p><hr class="mb-5">';
+
+  function makePost(title, post, id) {
+      return '<h2 class="mb-0">' + title + "</h2> <p style='font-size: 1.2rem' class='mb-3'>"
+              + post + '</p>'
+              + '<form data-id="' + id + '"class="commentary-form" method="post" enctype="multipart/form-data">'
+              + '<div class="form-group">'
+              + '<label for="commentary" class="sr-only">Commentary</label>'
+              + '<input type="text" class="form-control" id="commentary" placeholder="Enter your commentary">'
+              + '</div></form><hr class="my-5">'
+;
     }
   var postsElement = $("#posts");
 
@@ -28,27 +35,39 @@ $(function() {
     url: "api.php?category=ALL",
   }).done(function(data) {
     $.each(data, function(key, value) {
+      console.log('id is ' + value['id']);
+      var id = value['id'];
       var title = value["title"];
       var post = value["post"];
-      var post = makePost(title, post);
+      var post = makePost(title, post, id);
       postsElement.append(post);
+    })
+
+    var forms = $(".commentary-form");
+    forms.submit(function(event) {
+      event.preventDefault();
+      var commentData = {};
+      commentData['comment'] = $(this).find('input').val();
+      commentData['article_id'] = $(this).data('id');
+      $.ajax({
+        method: 'POST',
+        url: "commentary_api.php",
+        data: commentData
+      }).done(function() {
+
+      })
     })
   })
 
   function getCategoryPosts() {
     $(".category-menu").click(function() {
-      console.log($(this));
       var category = $(this)[0].getAttribute("id");
       postsElement.html("");
       console.log("api.php?category=" + category);
       $.ajax({
         method: "GET",
         url: "api.php?category=" + category,
-        success: function(data){
-          console.log("Hello!");
-        }
       }).done(function(data) {
-        console.log("Hello!");
         console.log(data);
 
         $.each(data, function(key, value) {
@@ -59,5 +78,4 @@ $(function() {
       })
     })
   }
-
 })
