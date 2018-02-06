e1c9936e30f5c4440a47dffec1cc1c997cf958e7
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-06 11:08:20 +0100

    Made the code shorter and easier to read.

--- getCategories.js
+++ getCategories.js
@@ -2,17 +2,13 @@ $(function() {
   function makeCategory(category_id, category) {
     return '<option value="' + category_id + '">' + category + '</option>';
   }
-  var categoriesElement = $("#categoriesSelector");
+  
   $.ajax({
     method: "GET",
     url: "categories_api.php",
   }).done(function(data) {
     $.each(data, function(key, value) {
-      var category = value["category"];
-      var category_id = value["id"];
-      console.log(category_id);
-      var categoryOption = makeCategory(category_id, category);
-      categoriesElement.append(categoryOption);
+      $("#categoriesSelector").append(makeCategory(value["id"], value["category"]));
     })
   })
 })
