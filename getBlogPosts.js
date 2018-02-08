179902348900b01b24d365c338d7edd29fb1ed82
heinerbehrends <heinerbehrends@hotmail.com>
2018-02-08 14:06:26 +0100

    Changed makePosts to conditionally include comments. Added click handler for comments_on_off interface

--- getBlogPosts.js
+++ getBlogPosts.js
@@ -9,18 +9,29 @@ $(function() {
     '<p class="commentParagraph">' + comment + '</p>'
   }
 
-  function makePost(title, post, id) {
-      return '<h2 class="mb-0">' + title + '</h2>'
-              + "<p style='font-size: 1.2rem' class='mb-3'>" + post + '</p>'
-              + '<h3>Comments</h3>'
-              + '<div id="' + id + '" class="comment"></div>'
-              + '<form data-id="' + id + '"class="commentary-form" method="post" enctype="multipart/form-data">'
-              + '<div class="form-group">'
-              + '<label for="commentary" class="sr-only">Commentary</label>'
-              + '<input type="text" class="form-control" id="commentary" placeholder="Enter your commentary">'
-              + '</div></form><hr class="my-5">'
-;
+  function makePost(title, post, id, comments_on_off) {
+    var post_string = '<h2 class="mb-0">' + title + '</h2>'
+    + "<p style='font-size: 1.2rem' class='mb-3'>" + post + '</p>'
+    if (comments_on_off) {
+      post_string += '<div class="comments_on_off" data-id="' + id + '" data-on_off="0">'
+      + '<img style="height: 30px" class="mr-3 my-3" src="on-off-button.svg">'
+      + '<span>Click here to turn off comments for this post</span></div>'
+      + '<h3 class="mt-3">Comments</h3>'
+      + '<div id="' + id + '" class="comment"></div>'
+      + '<form data-id="' + id + '"class="commentary-form" method="post" enctype="multipart/form-data">'
+      + '<div class="form-group">'
+      + '<label for="commentary" class="sr-only">Commentary</label>'
+      + '<input type="text" class="form-control" id="commentary" placeholder="Enter your commentary">'
+      + '</div></form><hr class="my-5">';
+      return post_string;
     }
+    else {
+      return post_string += '<div class="comments_on_off" data-id="' + id + '" data-on_off="1">'
+      + '<img style="height: 30px" class="mr-3 my-3" src="on-off-button.svg">'
+      + '<span>Click here to turn on comments for this post</span></div>'+ '<hr class="my-5">';
+    }
+  }
+
   var postsElement = $("#posts");
 
   $.ajax({
@@ -45,7 +56,8 @@ $(function() {
       var id = value['id'];
       var title = value["title"];
       var post = value["post"];
-      var post = makePost(title, post, id);
+      var comments_on_off = parseInt(value["comments_on_off"]);
+      var post = makePost(title, post, id, comments_on_off);
       postsElement.append(post);
     })
 
@@ -59,7 +71,8 @@ $(function() {
             $(this).append(
               '<div class="row">'
               + '<p class="comment-paragraph col">' + data[i]['comment'] + '</p>'
-              + '<div class="col-auto ml-auto delete-button"><img data-commentID="' + data[i]['comment_id'] + '" src="delete_icon" alt="delete icon" style="height:20px"></div>'
+              + '<div class="col-auto ml-auto delete-button">'
+              + '<img data-commentID="' + data[i]['comment_id'] + '" src="delete_icon" alt="delete icon" style="height:15px"></div>'
               + '</div>'
             );
           }
@@ -73,7 +86,20 @@ $(function() {
           context: $(this),
           data: {comment_id: comment_id}
         }).done(function() {
-            $(this).parent().remove();
+          $(this).parent().remove();
+        })
+      })
+      $(".comments_on_off").click(function() {
+        var on_off = $(this).attr('data-on_off');
+        var post_id = $(this).attr('data-id');
+        $.ajax({
+          method: 'POST',
+          url: 'comments_on_off.php',
+          context: $(this),
+          dataType: 'text',
+          data: {on_off: on_off, post_id: post_id}
+        }).done(function(data) {
+          window.location.reload(true);
         })
       })
     })
@@ -110,8 +136,25 @@ $(function() {
       }).done(function(data) {
         $.each(data, function(key, value) {
           var postCategory = value["category"];
-          var post = makePost(value["title"], value["post"]);
+          var id = value['id'];
+          var title = value["title"];
+          var post = value["post"];
+          var comments_on_off = parseInt(value["comments_on_off"]);
+          var post = makePost(title, post, id, comments_on_off);
           postsElement.append(post);
+        })
+        $(".comments_on_off").click(function() {
+          var on_off = $(this).attr('data-on_off');
+          var post_id = $(this).attr('data-id');
+          $.ajax({
+            method: 'POST',
+            url: 'comments_on_off.php',
+            context: $(this),
+            dataType: 'text',
+            data: {on_off: on_off, post_id: post_id}
+          }).done(function(data) {
+            window.location.reload(true);
+          })
         })
       })
     })
